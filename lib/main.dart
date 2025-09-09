30be350fd9a849a7eb4bfa8b2784c6cf8c03d8e1
Ruben Steijn <therubenation@gmail.com>
2025-09-09 16:33:35 +0200

    fixed set up to new flutter best practices, not done

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,13 @@ import 'package:flash_chat_flutter_2025/screens/chat_screen.dart';
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.black54),
+          bodyMedium: TextStyle(color: Colors.black54),
         ),
       ),
       home: WelcomeScreen(),

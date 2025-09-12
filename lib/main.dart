07d17b80543cd26bd40278da55fb165ce79b4ce0
Ruben Steijn <therubenation@gmail.com>
2025-09-12 16:25:04 +0200

    successful first uild with firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flash_chat_flutter_2025/screens/welcome_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/login_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/chat_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/registration_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({super.key});

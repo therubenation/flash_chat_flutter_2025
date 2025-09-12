15711e02dce4fb571dd4a7c8cdcebc5253b97485
Ruben Steijn <therubenation@gmail.com>
2025-09-12 14:11:50 +0200

    refactor. Before firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat_flutter_2025/screens/welcome_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/login_screen.dart';
-import 'package:flash_chat_flutter_2025/screens/registration_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/chat_screen.dart';
+import 'package:flash_chat_flutter_2025/screens/registration_screen.dart';
 
 void main() => runApp(FlashChat());
 
@@ -11,11 +11,6 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyMedium: TextStyle(color: Colors.black54),
-        ),
-      ),
       initialRoute: WelcomeScreen.id,
       routes: {
         WelcomeScreen.id: (context) => const WelcomeScreen(),
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat_flutter_2025/screens/login_screen.dart';
 import 'package:flash_chat_flutter_2025/screens/registration_screen.dart';
+import 'package:flash_chat_flutter_2025/components/roundedButton.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -82,34 +83,16 @@ class _WelcomeScreenState extends State<WelcomeScreen>
               ],
             ),
             const SizedBox(height: 48.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () => Navigator.pushNamed(context, LoginScreen.id),
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text('Log In'),
-                ),
-              ),
+            RoundedButton(
+              title: 'Log In',
+              color: Colors.lightBlueAccent,
+              onPressed: () => Navigator.pushNamed(context, LoginScreen.id),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () =>
-                      Navigator.pushNamed(context, RegistrationScreen.id),
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text('Register'),
-                ),
-              ),
+            RoundedButton(
+              title: 'Register',
+              color: Colors.blueAccent,
+              onPressed: () =>
+                  Navigator.pushNamed(context, RegistrationScreen.id),
             ),
           ],
         ),
